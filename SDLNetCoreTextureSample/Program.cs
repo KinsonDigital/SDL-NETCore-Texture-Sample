68d2b89affcbba7f486603f5e6843cade84ecdd0
Calvin Wilkinson <kinsondigital@gmail.com>
2019-05-20 21:54:56 -0400

    Library setup

--- SDLNetCoreTextureSample/Program.cs
+++ SDLNetCoreTextureSample/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using SDL2;
+using System;
+using System.Threading;
 
 namespace SDLNetCoreTextureSample
 {
-    class Program
+    public class Program
     {
+        private const int WINDOW_WIDTH = 640;
+        private const int WINDOW_HEIGHT = 480;
+        private static bool _quit = false;
+        private static IntPtr _windowPtr;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialize SDL.  A true value means initialization was successful.
+            var isInitialized = Init();
+
+            Console.WriteLine("Press one of the arrow keys!");
+            Console.WriteLine();
+
+            //If successfully initialized.
+            if (isInitialized)
+            {
+                //This can represent your game loop
+                while (!_quit)
+                {
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Just Waiting. . . ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to initialize!");
+                Console.ReadLine();
+            }
+        }
+
+
+        private static bool Init()
+        {
+            //Init the video card
+            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
+            {
+                Console.WriteLine("SDL could not initialize! SDL_Error: {0}", SDL.SDL_GetError());
+                return false;
+            }
+            else
+            {
+                //Create an SDL window to render graphics upon.
+                _windowPtr = SDL.SDL_CreateWindow("SDL2 Keyboard Sample", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+                if (_windowPtr == IntPtr.Zero)
+                {
+                    Console.WriteLine("The window could not be created!!");
+                    Console.ReadLine();
+                    return false;
+                }
+            }
+
+
+            return true;
         }
     }
 }
